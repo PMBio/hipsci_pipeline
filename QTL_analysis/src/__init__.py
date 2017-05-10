05069603c1dd4d8d39cbd083298782fc7780700a
Daniel Seaton <daniel.seaton87@gmail.com>
2017-05-10 10:16:04 +0100

    Made load_genotypes a callable function in the module

--- QTL_analysis/src/__init__.py
+++ QTL_analysis/src/__init__.py
@@ -1,1 +1,2 @@
 
+from load_genotypes import load_genotypes
--- QTL_analysis/src/load_genotypes.py
+++ QTL_analysis/src/load_genotypes.py
@@ -1,34 +1,34 @@
 import limix.io
 import numpy as np
-import math
-import scipy
 
-(bim, fam, bed) = limix.io.read_plink('../data/geuvadis_test_data/Geuvadis_chr1')
+def load_genotypes_plink(plink_prefix):
+    '''Return the genotype from a set of plink files
+    
+    >>> plink_prefix = '../data/geuvadis_test_data/Geuvadis_chr1'
+    >>> genotypes = load_genotypes_plink(plink_prefix)
+    >>> genotypes[0,4]
+    2.0
+    >>> genotypes[1,-1]
+    2.0
+    >>> genotypes[-1,-4]
+    1.0
 
-#make missing values 0 for now (i.e. homozygous for first allele)
-#needs to be replaced with a snp-specific value
-missing_value = 0
+    '''
+    (bim, fam, bed) = limix.io.read_plink(plink_prefix)
 
-def bed_to_genotype_value(input_value):
-	value_mapping = [0,1,2,missing_value]
-	return value_mapping[input_value]
+    #make missing values 0 for now (i.e. homozygous for second allele)
+    #needs to be replaced with a snp-specific value
+    missing_value = 2
+    
+    def bed_to_genotype_value(input_value):
+    	value_mapping = [0,1,2,missing_value]
+    	return value_mapping[input_value]
+    
+    #(nN,nS)
+    genotypes = np.array(np.vectorize(bed_to_genotype_value)(bed.compute()),dtype=float).transpose()
+    
+    return genotypes
 
-#rows are individuals, SNPs are columns
-chrsnps = np.array(np.vectorize(bed_to_genotype_value)(bed.compute()),dtype=float).transpose()
-print(chrsnps[0:4,0:8])
-
-kchr = chrsnps
-#average by column and subtract
-kchr -= chrsnps.mean(axis=0)
-print(kchr)
-kchr /= kchr.std(axis=0)
-print(kchr)
-
-
-nS = chrsnps.shape[0]
-nN = chrsnps.shape[1]
-
-kinship = scipy.dot(kchr, kchr.T)
-
-print(kinship.shape)
-print(kinship)
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
