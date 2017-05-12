7ccc09de2796983c61e60bc514d670539d6d710c
danielseaton <daniel.seaton87@gmail.com>
2017-05-12 09:37:27 +0100

    Output all pandas_plink dataframes

--- limix_QTL_pipeline/limix_QTL_pipeline/load_genotypes.py
+++ limix_QTL_pipeline/limix_QTL_pipeline/load_genotypes.py
@@ -4,8 +4,8 @@ import numpy as np
 def load_genotypes_plink(plink_prefix):
     '''Return the genotype from a set of plink files
     
-    >>> plink_prefix = '../data/geuvadis_test_data/Geuvadis_chr1'
-    >>> genotypes = load_genotypes_plink(plink_prefix)
+    >>> plink_prefix = '../data/geuvadis_CEU_YRI_test_data/Geuvadis_chr1'
+    >>> bim,fam,bed,genotypes = load_genotypes_plink(plink_prefix)
     >>> genotypes[0,4]
     2.0
     >>> genotypes[1,-1]
@@ -27,7 +27,7 @@ def load_genotypes_plink(plink_prefix):
     #(nN,nS)
     genotypes = np.array(np.vectorize(bed_to_genotype_value)(bed.compute()),dtype=float).transpose()
     
-    return bim,genotypes
+    return bim,fam,bed,genotypes
 
 if __name__ == "__main__":
     import doctest
