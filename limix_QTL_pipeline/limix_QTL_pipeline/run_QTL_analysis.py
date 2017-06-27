70ce1bc0fe6c0678855c3014a7a54602e699e1f9
danielseaton <daniel.seaton87@gmail.com>
2017-06-27 16:48:43 +0100

    Outputs default values to the corrected p value field

--- limix_QTL_pipeline/limix_QTL_pipeline/run_QTL_analysis.py
+++ limix_QTL_pipeline/limix_QTL_pipeline/run_QTL_analysis.py
@@ -206,14 +206,14 @@ def run_QTL_analysis(pheno_filename,anno_filename,geno_prefix,plinkGenotype,wind
                     #Calculate P-value using beta dist.
 
                 #add these results to qtl_results
-                #temp_df = pd.DataFrame(index = range(len(snp_names)),columns=['feature_id','snp_id','p_value','beta','n_samples','corr_p_value'])
-                temp_df = pd.DataFrame(index = range(len(snp_names)),columns=['feature_id','snp_id','p_value','beta','n_samples'])
+                temp_df = pd.DataFrame(index = range(len(snp_names)),columns=['feature_id','snp_id','p_value','beta','n_samples','corr_p_value'])
                 temp_df['snp_id'] = snp_names
                 temp_df['feature_id'] = feature_id
                 temp_df['beta'] = LMM.getBetaSNP()[0]
                 temp_df['p_value'] = LMM.getPv()[0]
                 temp_df['n_samples'] = sum(~np.isnan(phenotype))
-                #temp_df['corr_p_value'] = sum(~np.isnan(phenotype))
+                #insert default dummy value
+                temp_df['corr_p_value'] = 1.0
 
                 results_df = results_df.append(temp_df)
             if not results_df.empty :
