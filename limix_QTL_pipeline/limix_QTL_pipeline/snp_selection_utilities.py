8d493cd4c8703ccb94c0fec81209b19f94e5e7ef
danielseaton <daniel.seaton87@gmail.com>
2017-09-24 18:30:23 +0100

    multi-region selection will work for trans as well. Issue: when running on a specific chromosome, any feature with any region on that chromosome will be included. This means some features will be run multiple times, especially when selecting multiple regions for trans analysis (since they will usually be on different chromosomes).

--- limix_QTL_pipeline/limix_QTL_pipeline/snp_selection_utilities.py
+++ limix_QTL_pipeline/limix_QTL_pipeline/snp_selection_utilities.py
@@ -11,12 +11,17 @@ def get_snps(feature_id, annotation_df, bim, cis_mode, window_size):
         lowest = min([start,end])
         highest = max([start,end])
         if (cis_mode) :
+            # for cis, we sequentially add snps that fall within each region
             snpQuery = bim.query("chrom == '%s' & pos > %d & pos < %d" % (chrom, lowest-window_size, highest+window_size))
+            list_of_snp_dfs.append(snpQuery)
         else :
-            snpQuery = bim.query("(chrom == '%s' & (pos < %d | pos > %d))|chrom != '%s'" % (chrom, lowest-window_size, highest+window_size,chrom))
-            #Filtering for sites on non allosomes.
-            snpQuery = selfnpQuery.loc[snpQuery['chrom'].map(lambda x: x in list(map(str, range(1, 23))))]
-        list_of_snp_dfs.append(snpQuery)
-    selected_snp_df = pd.concat(list_of_snp_dfs).drop_duplicates()
+            # for trans, we sequentially exclude snps that fall within each region
+            snpQuery = snpQuery.query("(chrom == '%s' & (pos < %d | pos > %d))|chrom != '%s'" % (chrom, lowest-window_size, highest+window_size,chrom))
+    if (cis_mode):
+        selected_snp_df = pd.concat(list_of_snp_dfs).drop_duplicates()
+    else:
+        selected_snp_df = snpQuery
+    # filtering for sites on non allosomes.
+    selected_snp_df = selected_snp_df.loc[selected_snp_df['chrom'].map(lambda x: x in list(map(str, range(1, 23))))]
     
     return selected_snp_df
