4302ac7078fceb11655fd2fd8efcd158ece56e4f
Marc Jan Bonder <bonder.m.j@gmail.com>
2017-09-25 12:10:47 +0100

    Renamend and updates snp_selection function

--- limix_QTL_pipeline/limix_QTL_pipeline/qtl_snp_selection.py
+++ limix_QTL_pipeline/limix_QTL_pipeline/qtl_snp_selection.py
@@ -3,6 +3,7 @@ import pandas as pd
 def get_snps(feature_id, annotation_df, bim, cis_mode, window_size):
     annotation_sub_df = annotation_df.loc[[feature_id],:]
     list_of_snp_dfs = []
+    snpQuery = None
     for feature_id,annotation_ds in annotation_sub_df.iterrows():
         chrom = str(annotation_ds.loc['chromosome'])
         start = annotation_ds.loc['start']
@@ -16,7 +17,13 @@ def get_snps(feature_id, annotation_df, bim, cis_mode, window_size):
             list_of_snp_dfs.append(snpQuery)
         else :
             # for trans, we sequentially exclude snps that fall within each region
-            snpQuery = snpQuery.query("(chrom == '%s' & (pos < %d | pos > %d))|chrom != '%s'" % (chrom, lowest-window_size, highest+window_size,chrom))
+            if snpQuery is None :
+                #Initial search for trans window.
+                snpQuery = bim.query("(chrom == '%s' & (pos < %d | pos > %d))|chrom != '%s'" % (chrom, lowest-window_size, highest+window_size,chrom))
+            else :
+                #Here we start intersecting regions we want to exclude.
+                snpQuery = snpQuery.query("(chrom == '%s' & (pos < %d | pos > %d))|chrom != '%s'" % (chrom, lowest-window_size, highest+window_size,chrom))
+    
     if (cis_mode):
         selected_snp_df = pd.concat(list_of_snp_dfs).drop_duplicates()
     else:
