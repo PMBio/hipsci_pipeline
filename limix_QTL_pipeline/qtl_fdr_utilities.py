c4eb4b19a02076e1cfbf56c48902dc16befb70f1
Marc Jan Bonder <bonder.m.j@gmail.com>
2017-10-31 13:29:27 +0000

    Removed unnecessary test for above 1 and below 0 p-values

--- limix_QTL_pipeline/qtl_fdr_utilities.py
+++ limix_QTL_pipeline/qtl_fdr_utilities.py
@@ -17,9 +17,9 @@ def define_correction_function(top_pvalues_perm):
     #If the MLE estimator fails we go back to the cruder estimation of the beta distribution.
     offset = (np.finfo(np.double).tiny*100)
     ##Replace lowest value with smallest number not 0.
-    top_pvalues_perm[top_pvalues_perm <= 0] = offset
+    top_pvalues_perm[top_pvalues_perm == 0] = offset
     ##Replace highest value with highest number not 1.
-    top_pvalues_perm[top_pvalues_perm >= 1] = 1-offset
+    top_pvalues_perm[top_pvalues_perm == 1] = 1-offset
     try :
         alpha_para,beta_para,loc,fscale =  beta.fit(top_pvalues_perm,floc=0,fscale=1)
     except (scipy.stats._continuous_distns.FitSolverError):
@@ -28,6 +28,7 @@ def define_correction_function(top_pvalues_perm):
         alpha_para,beta_para = estimate_beta_function_paras(top_pvalues_perm)
     beta_dist = scipy.stats.beta(alpha_para,beta_para)
     correction_function = lambda x: beta_dist.cdf(x)
+    #Would be good to replace 0 with minimal double value of python.
     return correction_function
 
 def calculate_corrected_pvalues(top_pvalues_perm,nominal_pvalues):
