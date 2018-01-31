9612aa7efafd21b6e43316eab2e20a1a59236f1e
Marc Jan Bonder <bonder.m.j@gmail.com>
2018-01-31 13:27:06 +0000

    Minor ups

--- limix_QTL_pipeline/qtl_utilities.py
+++ limix_QTL_pipeline/qtl_utilities.py
@@ -501,7 +501,7 @@ def do_snp_selection(feature_id, annotation_df, bim, cis_mode, window_size, skip
         selected_snp_df = snpQuery
     if not skipAutosomeFiltering :
         # filtering for sites on non allosomes.
-        selected_snp_df = selected_snp_df.loc[selected_snp_df['chrom'].map(lambda x: x in list(map(str, range(1, 23))))]
+        selected_snp_df = selected_snp_df.loc[selected_snp_df['chrom'].map(lambda x: x in list(map(str, range(1, 23)))).values]
     
     return selected_snp_df
 
