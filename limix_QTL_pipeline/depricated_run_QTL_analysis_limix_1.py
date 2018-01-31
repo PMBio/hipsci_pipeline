3cb7e7542b57291355964ba2e1c270e0ce2d9dfa
Marc Jan Bonder <bonder.m.j@gmail.com>
2018-01-31 11:30:55 +0000

    Reduce SNPs before testing in LMM

--- limix_QTL_pipeline/depricated_run_QTL_analysis_limix_1.py
+++ limix_QTL_pipeline/depricated_run_QTL_analysis_limix_1.py
@@ -203,8 +203,9 @@ def run_QTL_analysis(pheno_filename, anno_filename, geno_prefix, plinkGenotype, 
                             temp = utils.get_shuffeld_genotypes_preserving_kinship(geneticaly_unique_individuals, relatedness_score, snp_matrix_DF,kinship_df.loc[individual_ids,individual_ids], len(currentNperm))
                         else :
                             temp = utils.get_shuffeld_genotypes(snp_matrix_DF, len(currentNperm))
-                        LMM_perm = limix.qtl.qtl_test_lmm(temp, phenotype,K=kinship_mat,covs=cov_matrix)
-                        pValueBuffer.extend(np.asarray(LMM_perm.variant_pvalues))
+                        reduceInfo  = utils.reduce_snp(temp)
+                        LMM_perm = limix.qtl.qtl_test_lmm(temp.loc[:,np.unique(reduceInfo['lead_snp_id'].values)], phenotype,K=kinship_mat,covs=cov_matrix)
+                        pValueBuffer.extend(np.asarray(LMM_perm.variant_pvalues[reduceInfo['lead_snp_id']]))
                     if(not(len(pValueBuffer)==totalSnpsToBeTested)):
                         #print(len(pValueBuffer))
                         #print(pValueBuffer)
